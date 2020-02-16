c483beb79440102970e35e5840ad3ea9ac582482
Mukhamed Talaspaev <7636muha@gmail.com>
2020-02-16 17:23:33 +0300

    feat:add basic nav.module.css

--- src/Components/Navbar/nav.jsx
+++ src/Components/Navbar/nav.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
+import s from './nav.module.css'
+
 const NavBar = () => {
   return (
-    <nav className="nav">
-      <div>
+    <nav className={s.nav}>
+      <div className={s.item}>
         <a>Profile</a>
       </div>
-      <div>Message</div>
-      <div>News</div>
-      <div>Music</div>
-      <div>Settings</div>
+      <div className={`${s.item } ${s.active}`}>
+        <a>Message</a>
+      </div>
+      <div className={s.item}>
+        <a>News</a>
+      </div>
+      <div className={s.item}>
+        <a>Music</a>
+      </div>
+      <div className={s.item}>
+        <a>Settings</a>
+      </div>
     </nav>
   );
 };
