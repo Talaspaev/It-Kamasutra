8f44487913e9aa8a45260f16b7b090417719ed34
Mukhamed Talaspaev <7636muha@gmail.com>
2020-02-18 19:19:52 +0300

    feat:dialogMessage

--- src/Components/Dialogs/dialogs.jsx
+++ src/Components/Dialogs/dialogs.jsx
@@ -6,13 +6,19 @@ import I18Y from '../../Core/constants';
 const Dialogs = () => (
   <div className={style.dialogs}>
     <div className={style.dialogsItems}>
-      <div className={style.item}>{I18Y.name[0]}</div>
-      <div className={style.item}>{I18Y.name[1]}</div>
-      <div className={style.item}>{I18Y.name[2]}</div>
-      <div className={style.item}>{I18Y.name[3]}</div>
-      <div className={style.item}>{I18Y.name[4]}</div>
+      <div className={style.dialog}>{I18Y.name[0]}</div>
+      <div className={style.dialog}>{I18Y.name[1]}</div>
+      <div className={style.dialog}>{I18Y.name[2]}</div>
+      <div className={style.dialog}>{I18Y.name[3]}</div>
+      <div className={style.dialog}>{I18Y.name[4]}</div>
     </div>
-
+    <div className={style.dialogsMessage}>
+      <div className={style.message}>{I18Y.message[0]}</div>
+      <div className={style.message}>{I18Y.message[1]}</div>
+      <div className={style.message}>{I18Y.message[2]}</div>
+      <div className={style.message}>{I18Y.message[3]}</div>
+      <div className={style.message}>{I18Y.message[4]}</div>
+    </div>
   </div>
 );
 
--- src/Core/constants.js
+++ src/Core/constants.js
@@ -2,6 +2,7 @@ const I18Y = {
   hi: 'Hello, my dear',
   qus: 'How are you?',
   ans: 'I am exhausted',
+  message: ['Lorem, ipsum dolor sit amet  ', ' praesentium tempore atque eveniet', 'architecto Placeat inventore officiis odit ipsa quibusdam', ' praesentium tempore atque eveniet', 'consectetur adipisicing elit. Officiis error'],
   name: ['Sergei', 'Andrey', 'Natalya', 'Igor', 'Egor'],
 };
 
