ababb75f54be58a8ed44b76c7edb9d1badc7a304
Mukhamed Talaspaev <7636muha@gmail.com>
2020-02-18 19:12:46 +0300

    feat:add diolog items

--- src/Components/Dialogs/dialogs.jsx
+++ src/Components/Dialogs/dialogs.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
 import style from './dialogs.module.css';
+import I18Y from '../../Core/constants';
 
 const Dialogs = () => (
-  <div className={style.profile}>
-    Dialogs ve been here
+  <div className={style.dialogs}>
+    <div className={style.dialogsItems}>
+      <div className={style.item}>{I18Y.name[0]}</div>
+      <div className={style.item}>{I18Y.name[1]}</div>
+      <div className={style.item}>{I18Y.name[2]}</div>
+      <div className={style.item}>{I18Y.name[3]}</div>
+      <div className={style.item}>{I18Y.name[4]}</div>
+    </div>
+
   </div>
 );
 
--- src/Core/constants.js
+++ src/Core/constants.js
@@ -2,6 +2,7 @@ const I18Y = {
   hi: 'Hello, my dear',
   qus: 'How are you?',
   ans: 'I am exhausted',
+  name: ['Sergei', 'Andrey', 'Natalya', 'Igor', 'Egor'],
 };
 
 export default I18Y;
