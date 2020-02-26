db9199101e6da2e47020bad23817b453217128bf
Mukhamed Talaspaev <Mukhamed_Talaspaev@epam.com>
2020-02-26 18:50:31 +0300

    fix:nav map func

---
+++ src/Components/Navbar/NavItem/navMapFunc.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import style from '../nav.module.css';
+
+const NavMapFunc = (props) => {
+  const { context } = props;
+  return (context.map((item) => (<div key={item.id} className={style.item}><NavLink to={item.path} activeClassName={style.active}>{item.name}</NavLink></div>)));
+};
+export default NavMapFunc;
--- src/Components/Navbar/nav.jsx
+++ src/Components/Navbar/nav.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
+import NavItemBlock from './NavItem/navMapFunc';
 import NavItem from './NavItem/navItem';
 
 import style from './nav.module.css';
 
 const NavBar = () => {
   const Items = Object.values(NavItem);
-  const NavItemBlock = Items.map((item) => (<div key={item.id} className={style.item}><NavLink to={item.path} activeClassName={style.active}>{item.name}</NavLink></div>));
   return (
     <nav className={style.nav}>
-      {NavItemBlock}
+      <NavItemBlock context={Items} />
     </nav>
   );
 };
