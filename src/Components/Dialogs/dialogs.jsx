c7dbc2f345f4bb7ac3ffac23d81117e796018842
Mukhamed Talaspaev <7636muha@gmail.com>
2020-03-15 23:47:35 +0300

    refactoring dialog

--- src/Components/Dialogs/dialogs.jsx
+++ src/Components/Dialogs/dialogs.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import I18Y from '../../core/constants';
 import GetItem from './GetItems/getItem';
 
 import style from './dialogs.module.css';
 
+const dialogsData = [{ id: 1, name: 'Dymuch' }, { id: 2, name: 'Andrey' }, { id: 3, name: 'Natalya' }, { id: 4, name: 'Igor' }, { id: 5, name: 'Egor' }];
+const GetPerson = (props) => {
+  const { numbers } = props;
+  const listItems = Object.keys(numbers).map((elem) => <NavLink to="dialogs/" className={style.item}>{numbers[elem].name}</NavLink>);
+  return (
+    listItems
+  );
+};
 const Dialogs = () => (
   <div className={style.dialogs}>
     <div className={style.dialogsItems}>
-      <GetItem numbers={I18Y.name} />
+      <GetPerson numbers={dialogsData} />
     </div>
     <div className={style.dialogsMessage}>
       <GetItem numbers={I18Y.message} />
--- src/Core/constants.js
+++ src/Core/constants.js
@@ -2,7 +2,7 @@ const I18Y = {
   hi: 'Hello, my dear',
   qus: 'How are you?',
   ans: 'I am exhausted',
-  number: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  number: [1, 2, 3, 4, 5, 6, 7, 8, 9],
   message: ['Lorem, ipsum dolor sit amet  ', ' praesentium tempore atque eveniet', 'architecto Placeat inventore officiis odit ipsa quibusdam', ' praesentium tempore atque eveniet', 'consectetur adipisicing elit. Officiis error'],
   name: ['Sergei', 'Andrey', 'Natalya', 'Igor', 'Egor'],
 };
