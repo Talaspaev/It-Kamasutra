217273aa1bb781d80c0bf12b143138825552a82e
Mukhamed Talaspaev <7636muha@gmail.com>
2020-05-01 20:23:29 +0300

    feat:added axios

--- src/Components/Users/users.jsx
+++ src/Components/Users/users.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import * as axios from 'axios';
 
 import { I18N } from '../../constants';
-import usersArray from '../../usersArray';
 
 import style from './users.module.css';
 
 const users = (props) => {
-  if (props.users.length === 0) {
-    props.setUsers(usersArray);
-  }
+  const loadUsers = () => {
+    if (props.users.length === 0) {
+      axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
+        props.setUsers(response.data.items);
+      });
+    }
+  };
   const toggleButton = (followed, id) => (followed ? <button type="button" onClick={() => { props.unfollowUser(id); }}>{I18N.ENG.UNFOLLOW}</button>
     : <button type="button" onClick={() => { props.followUser(id); }}>{I18N.ENG.FOLLOW}</button>);
 
   const usersItem = ({
-    id, photoUrl, followed, fullname, status, location,
+    id, photoUrl, followed, fullname, status,
   }) => (
     <div key={id} className={style.item}>
       <div>
@@ -25,16 +29,13 @@ const users = (props) => {
           {fullname}
           {status}
         </div>
-        <div>
-          {location.city}
-          {location.Country}
-        </div>
       </div>
     </div>
   );
   const usersContainer = props.users.map(usersItem);
   return (
     <div>
+      <button onClick={loadUsers}>Load</button>
       {usersContainer}
     </div>
 
