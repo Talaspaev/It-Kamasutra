e40fce20b8944be336c4e6e54db7a0e965099fc2
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-05-02 20:09:37 +0300

    Fix class properties and methods

--- src/Components/Users/users.jsx
+++ src/Components/Users/users.jsx
@@ -1,77 +1,127 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React from 'react';
-import * as axios from 'axios';
+import React from "react";
+import * as axios from "axios";
 
-import { I18N } from '../../constants';
+import { I18N } from "../../constants";
 
-import style from './users.module.css';
+import style from "./users.module.css";
 
-import defaultImg from '../Image/person.jpg';
+import defaultImg from "../Image/person.jpg";
 
 class Users extends React.Component {
+  state = {
+    pages: [],
+  };
+
   componentDidMount() {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-      .then((response) => {
-        this.props.setUsers(response.data.items);
-        this.props.setTotalUsersCount(response.data.totalCount);
+    const { currentPage, pageSize, setUsers, setTotalUsersCount } = this.props;
+
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`
+      )
+      .then(({ data }) => {
+        const { items, totalCount } = data;
+
+        setUsers(items);
+        setTotalUsersCount(totalCount);
+        this.getPages();
       });
   }
 
-  render() {
-    const pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+  getPages = () => {
     const pages = [];
-
-    for (let i = 1; i <= pagesCount; i++) {
+    for (let i = 1; i <= this.pagesCount(); i++) {
       pages.push(i);
     }
-    const onPageSelected = (pageNumber) => {
-      this.props.setCurrentPage(pageNumber);
-      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-        .then((response) => {
-          this.props.setUsers(response.data.items);
-        });
-    };
-    const createSpans = () => (
+
+    this.setState({ pages });
+  };
+
+  onPageSelected = (pageNumber) => {
+    const { setCurrentPage, pageSize, setUsers } = this.props;
+
+    setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${pageSize}`
+      )
+      .then(({ data }) => {
+        const { items } = data;
+
+        setUsers(items);
+      });
+  };
+
+  createSpans = () => (
+    <div>
+      {this.state.pages.map((p ,index) => (
+        <span
+          key={index}
+          className={
+            this.props.currentPage === p && style.selected
+              ? style.selected
+              : style.defaultspan
+          }
+          onClick={() => {
+            this.onPageSelected(p);
+          }}
+        >
+          {p}
+        </span>
+      ))}
+    </div>
+  );
+
+  toggleButton = (followed, id) => {
+    const { unfollowUser, followUser } = this.props;
+
+    return followed ? (
+      <button type="button" onClick={() => unfollowUser(id)}>
+        {I18N.ENG.UNFOLLOW}
+      </button>
+    ) : (
+      <button type="button" onClick={() => followUser(id)}>
+        {I18N.ENG.FOLLOW}
+      </button>
+    );
+  };
+
+  usersItem = ({ id, photos, followed, name, status }) => (
+    <div key={id} className={style.item}>
       <div>
-        {pages.map((p) => (
-          <span
-            className={this.props.currentPage === p && style.selected ? style.selected : style.defaultspan}
-            onClick={() => { onPageSelected(p); }}
-          >
-            {p}
-          </span>
-        ))}
+        <img
+          src={photos.small !== null ? photos.small : defaultImg}
+          alt=""
+          className={style.img}
+        />
+        {this.toggleButton(followed, id)}
       </div>
-    );
-
-    const toggleButton = (followed, id) => (followed ? <button type="button" onClick={() => { this.props.unfollowUser(id); }}>{I18N.ENG.UNFOLLOW}</button>
-      : <button type="button" onClick={() => { this.props.followUser(id); }}>{I18N.ENG.FOLLOW}</button>);
-
-    const usersItem = ({
-      id, photos, followed, name, status,
-    }) => (
-      <div key={id} className={style.item}>
+      <div>
         <div>
-          <img src={photos.small !== null ? photos.small : defaultImg} alt="" className={style.img} />
-          {toggleButton(followed, id)}
-        </div>
-        <div>
-          <div>
-            {name}
-            {status}
-          </div>
+          {name}
+          {status}
         </div>
       </div>
-    );
-    const usersContainer = this.props.users.map(usersItem);
+    </div>
+  );
+
+  pagesCount = () => {
+    const { totalUsersCount, pageSize } = this.props;
+
+    return Math.ceil(totalUsersCount / pageSize);
+  };
+
+  usersContainer = () => this.props.users.map(this.usersItem);
+
+  render() {
     return (
       <div>
-        {createSpans()}
-        <div>{usersContainer}</div>
+        {this.createSpans()}
+        <div>{this.usersContainer()}</div>
       </div>
-
     );
   }
 }
